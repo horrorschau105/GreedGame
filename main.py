2f25d98b602ac0b2108a807fec3819d4ed3d28af
horrorschau105 <piotrekgdowski@wp.pl>
2017-07-08 22:32:58 +0200

    looking for ideas

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 from grid import Grid
 import sys
 if __name__ == "__main__":
+	#workplan:
+	#
+	#import all grids
+	#import all ai-methods
+		#greed
+		#snail
+		#!greed
+		#greedAfterNsteps
+		#random
+		#tbd
+	#run methods
+	#print results
 	if(len(sys.argv) != 5):
 		print("Usage: python3 main.py (count of grids) (width) (height) (max value in simgle cell) > (output file)")
 		sys.exit()
