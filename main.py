514404463e148cbc81b9a6e978be5e62f6621b0f
horrorschau105 <piotrekgdowski@wp.pl>
2017-07-08 22:27:31 +0200

    added info about count of grids

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ if __name__ == "__main__":
 	if(len(sys.argv) != 5):
 		print("Usage: python3 main.py (count of grids) (width) (height) (max value in simgle cell) > (output file)")
 		sys.exit()
+	print(count)
 	count, width, height, maxvalue = [int(i) for i in sys.argv[1:]]
 	for i in range(count):
 		g = Grid(height, width, maxvalue)
