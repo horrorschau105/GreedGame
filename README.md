55a94fca49b6f52117e0685bb1f33d86c729d8e0
horrorschau105 <piotrekgdowski@wp.pl>
2017-07-15 22:52:33 +0200

    added avg steps info to summary

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # AI
 
-#### After spending hundreds hours on playing, let's try to write a bot playing game and beating our highscores :)
+## After spending hundreds hours on playing, let's try to write a bot playing game and beating our highscores :)
 
-##### Interface
+### Interface
 
 After every move, you get the info about:
 + possible direction of move
@@ -15,7 +15,7 @@ In **grid.py** method _data_ returns all needed data
 Then, you are to return one of 4 values - 0, 1, 2 or 3, meaning following directions: up, down, right left
 If you try to do an invalid move, a play on this grid will end, and next grid will be tested.
 
-##### Test
+### Test
 
 Run 
 ```
@@ -23,7 +23,7 @@ python3 main.py
 ``` 
 for testing all methods declared in **ai.py**
 
-##### Create
+### Create
 In **ai.py** there are some functions implementing simply tactics.
 There you can add yours.
 In **main.py** 
@@ -32,17 +32,17 @@ methods = [random, first, greed]
 ```
 _methods_ contains all names of method to test. Put there everything you want to test
 
-##### Summary
+### Summary
 
 Example of result:
 ```
-Name    Avg(%)  Min(%)  Max(%)  StDev   Fail
-random  2.042   0.308   6.119   1.295   0
-first   9.572   0.385   24.842  5.585   0
-greed   2.827   0.366   10.647  1.722   0
+Name    Avg(%)  Min(%)  Max(%)  StDev   Fail AvgSteps
+random  2.042   0.308   6.119   1.295   0    34.21
+first   9.572   0.385   24.842  5.585   0    50.33
+greed   2.827   0.366   10.647  1.722   0    31.11
 ```
 Column **Fail** indicates how many times your bot tried to move incorrectly. 
 
-##### Grids
+#### Grids
 
 File **grids.txt** contains 100 generated grids. See **generator** branch for more info
--- testyourai.py
+++ testyourai.py
@@ -6,33 +6,37 @@ class TestYourAI:
 	def __init__(self, grids):
 		self.grids = grids
 		print("Grids imported!")
-	def addToSummary(self, name, arr):
+	def addToSummary(self, name, array):
+		arr = [i[0] for i in array]
 		fails = len([i for i in arr if i==-1])
+		avg_steps = sum([i[1] for i in array if i[0] > 0]) / (len(arr)-fails)
 		avg = sum([i for i in arr if i>0]) / (len(arr)-fails)
 		varr = sum([(i-avg)*(i-avg) for i in arr if i>0]) / (len(arr)-fails)
 		sd = math.sqrt(varr)
 		minimum = min([i  for i in arr if i>0])
 		maximum = max(arr)
-		self.results.append([name, avg, minimum, maximum, sd, fails])
+		self.results.append([name, avg, minimum, maximum, sd, fails, avg_steps])
 	def testWith(self, method):
 		results = []
 		grids = deepcopy(self.grids)
 		for grid in grids:
+			steps = 0
 			while(True):
+				steps +=1
 				possible = grid.chkMove()
 				if(not any(possible)): #gameover
 					break
 				idx = method(grid.data())
 				if(not possible[idx]): #illegal move 
-					results.append(-1) #mark it
+					results.append([-1, -1]) #mark it
 					break
 				grid.move(idx)
-			results.append(grid.result*100 / grid.max)
+			results.append([grid.result*100 / grid.max, steps])
 		self.addToSummary(method.__name__, results)
 	def printResults(self):
-		print("Name\tAvg(%)\tMin(%)\tMax(%)\tStDev\tFail")
+		print("Name\tAvg(%)\tMin(%)\tMax(%)\tStDev\tFail\tAvgSteps")
 		#print(self.results)
 		for line in self.results:
-			print ("{}\t{:05.3f}\t{:05.3f}\t{:05.3f}\t{:05.3f}\t{}".format(line[0], line[1], line[2], line[3], line[4], line[5]))
+			print ("{}\t{:05.3f}\t{:05.3f}\t{:05.3f}\t{:05.3f}\t{}\t{:05.3f}".format(line[0], line[1], line[2], line[3], line[4], line[5], line[6]))
 		
 	
