96b7a1a9350773e9aa5da56a1fd7e24d04c62e81
horrorschau105 <piotrekgdowski@wp.pl>
2017-07-20 23:18:09 +0200

    snail boosted to avg 26%

--- ai.py
+++ ai.py
@@ -1,69 +1,62 @@
 from grid import Grid
 from examples import first
 #here you can put any ai method with additional classes 
-class goodsnail:
-#         <- 2
-#      3 XXXXXX ^
-#      I XXXXXX I
-#      v XXXXXX 1
-#         -> 0
+class combo:
+#        <-2
+#      3 XX ^
+#      v XX 1
+#        0->
 	direction, mostup, mostdown, mostright, mostleft = 0,0,0,0,0
 	def setDirection(grid):
-		goodsnail.mostup, goodsnail.mostdown, goodsnail.mostright, goodsnail.mostleft = -1, grid.height+1, grid.width+1, -1
+		combo.mostup, combo.mostdown, combo.mostright, combo.mostleft = -1, grid.height+1, grid.width+1, -1
 		if grid.position[0] > grid.height/2 and grid.position[1] > grid.width/2:
-			goodsnail.direction = 0
+			combo.direction = 0
 		if grid.position[0] < grid.height/2 and grid.position[1] < grid.width/2:
-			goodsnail.direction = 2
+			combo.direction = 2
 		if grid.position[0] < grid.height/2 and grid.position[1] > grid.width/2:
-			goodsnail.direction = 1
+			combo.direction = 1
 		if grid.position[0] > grid.height/2 and grid.position[1] < grid.width/2:
-			goodsnail.direction = 3
-	def snail2(grid):
+			combo.direction = 3
+		wiseBFS.setN(grid)
+	def combo(grid):
+		if grid.score() > 20:
+			return wiseBFS.moveN(grid)
 		up, down, right, left = grid.chkMove()
-		if goodsnail.direction == 0:
-			if grid.position[0] > goodsnail.mostdown and up: return 0 
+		if combo.direction == 0:
+			if grid.position[0] > combo.mostdown and up: return 0 
 			if down: return 1
 			if right: return 2
-			goodsnail.direction = 1
-			goodsnail.mostdown = grid.position[0]
-		elif goodsnail.direction == 1:
-			if grid.position[1] > goodsnail.mostright and left: return 3
+			combo.direction = 1
+			combo.mostdown = grid.position[0]
+		elif combo.direction == 1:
+			if grid.position[1] > combo.mostright and left: return 3
 			if right: return 2
 			if up: return 0
-			goodsnail.direction = 2
-			goodsnail.mostright = grid.position[1]
-		elif goodsnail.direction == 2:
-			if grid.position[0] < goodsnail.mostup and down: return 1
+			combo.direction = 2
+			combo.mostright = grid.position[1]
+		elif combo.direction == 2:
+			if grid.position[0] < combo.mostup and down: return 1
 			if up: return 0
 			if left: return 3
-			goodsnail.direction = 3
-			goodsnail.mostup = grid.position[0]
-		elif goodsnail.direction == 3:
-			if grid.position[1] < goodsnail.mostleft and right: return 2
+			combo.direction = 3
+			combo.mostup = grid.position[0]
+		elif combo.direction == 3:
+			if grid.position[1] < combo.mostleft and right: return 2
 			if left: return 3
 			if down: return 1
-			goodsnail.direction = 0
-			goodsnail.mostleft = grid.position[0]
+			combo.direction = 0
+			combo.mostleft = grid.position[0]
 		return first(grid)
 class wiseBFS: # same as deepgreed, but with deep manipulation
 	deep = 1
 	steps = []
 	def setN(grid):
-		wiseBFS.deep = 3
+		wiseBFS.deep = 5
 	def moveN(grid):
 		if len(wiseBFS.steps) == 0:
 			wiseBFS.steps = wiseBFS.bfsPath(grid, wiseBFS.deep)[0]
 		move = wiseBFS.steps[0]
 		wiseBFS.steps = wiseBFS.steps[1:] 
-		score = grid.score()
-		if score > 20:
-			wiseBFS.deep = 9
-		elif score > 17:
-			wiseBFS.deep = 7
-		elif score > 12:
-			wiseBFS.deep = 5
-		elif score > 5:
-			wiseBFS.deep = 4
 		return move
 	def bfsPath(grid, deep):
 		if deep == 0:
@@ -90,5 +83,49 @@ class wiseBFS: # same as deepgreed, but with deep manipulation
 				left = wiseBFS.bfsPath(grid, deep - 1)
 				left[0]  = [3] + left[0]
 				grid.undoLeft()
-			#print ([up, down, right, left], grid.position)
 			return [[mv, res + grid.result] for mv, res in [up, down, right, left] if 	res == max([tup[1] for tup in [up, down, right	, left]])][0]
+class boostedsnail:
+#        <-2
+#      3 XX ^
+#      v XX 1
+#        0->
+	direction, mostup, mostdown, mostright, mostleft = 0,0,0,0,0
+	def setDirection(grid):
+		boostedsnail.mostup, boostedsnail.mostdown, boostedsnail.mostright, boostedsnail.mostleft = -1, grid.height+1, grid.width+1, -1
+		distup, distdown, distright, distleft = grid.position[0], grid.height - grid.position[0], grid.width - grid.position[1], grid.position[1]
+		distances = [distup, distdown, distright, distleft]
+		if distup == min(distances):
+			boostedsnail.direction = 2
+		if distdown == min(distances):
+			boostedsnail.direction = 0
+		if distright == min(distances):
+			boostedsnail.direction = 1
+		if distleft == min(distances):
+			boostedsnail.direction = 3
+	def Bsnail(grid):
+		up, down, right, left = grid.chkMove()
+		if boostedsnail.direction == 0:
+			if grid.position[0] > boostedsnail.mostdown and up: return 0 
+			if right: return 2
+			boostedsnail.direction = 1
+			boostedsnail.mostdown = grid.position[0]
+			if up: return 0
+		elif boostedsnail.direction == 1:
+			if grid.position[1] > boostedsnail.mostright and left: return 3
+			if up: return 0
+			boostedsnail.direction = 2
+			boostedsnail.mostright = grid.position[1]
+			if left: return 3
+		elif boostedsnail.direction == 2:
+			if grid.position[0] < boostedsnail.mostup and down: return 1
+			if left: return 3
+			boostedsnail.direction = 3
+			boostedsnail.mostup = grid.position[0]
+			if down: return 1
+		elif boostedsnail.direction == 3:
+			if grid.position[1] < boostedsnail.mostleft and right: return 2
+			if down: return 1
+			boostedsnail.direction = 0
+			boostedsnail.mostleft = grid.position[0]
+			if right: 2
+		return first(grid)
